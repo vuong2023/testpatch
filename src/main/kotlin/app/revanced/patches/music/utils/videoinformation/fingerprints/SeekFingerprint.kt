2feb3299b87fa443fd37ef40f7a57bc4222a8dad
vuong2023 <124447749+vuong2023@users.noreply.github.com>
2023-09-18 06:45:14 +0700

    Delete src/main directory

--- src/main/kotlin/app/revanced/patches/music/utils/videoinformation/fingerprints/SeekFingerprint.kt
+++
@@ -1,7 +0,0 @@
-package app.revanced.patches.music.utils.videoinformation.fingerprints
-
-import app.revanced.patcher.fingerprint.method.impl.MethodFingerprint
-
-object SeekFingerprint : MethodFingerprint(
-    strings = listOf("Attempting to seek during an ad")
-)
